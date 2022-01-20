a8b409a3953209c24381d03df5f9fbce9fcaa4a1
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-01-20 19:27:48 +0600

    my cart backend

--- components/Cart/ProductCart.js
+++ components/Cart/ProductCart.js
@@ -5,20 +5,27 @@ import model from './../styles/model';
 import {Checkbox} from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-const ProductCart = () => {
+const ProductCart = (props) => {
     const [ProductName,setProductName] =React.useState('Frouits');
     const [ProductPrize,setProductPrize] =React.useState('200');
     const [Check,setCheck] = React.useState(false);
     return (
         <DropShadow style={model.shadow}>
             <View style={{ backgroundColor: '#ffff',flexDirection:'row',alignItems: 'center',margin:5,borderRadius:10}}>
-            <Checkbox status={Check? 'checked' : 'unchecked'} onPress={() =>setCheck(!Check)}/>
+            <Checkbox status={Check? 'checked' : 'unchecked'} onPress={() =>{
+                setCheck(!Check)
+                if(Check){
+                    props.select(props.id)
+                }else{
+                    props.unselect(props.id)
+                }
+            }}/>
                 <Image style={[model.image]} source={require('./../Files/playstore.png')}/>
                 <View style={{flex:2}}>
                     <Text style={{fontSize:17}}>{ProductName}</Text>
                     <Text style={{fontSize:13,color:'tomato'}}>{ProductPrize} Tk</Text>
                 </View>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() =>props.delete(props.id)}>
                 <MaterialCommunityIcons style={{margin:15}} name="delete" size={25}/>
                 </TouchableOpacity>
             </View>
--- components/User/MyCart.js
+++ components/User/MyCart.js
@@ -3,17 +3,57 @@ import{View, Text,ScrollView} from 'react-native'
 import { Button } from 'react-native-paper';
 import ProductCart from './../Cart/ProductCart'
 import model from './../styles/model';
-const MyCart = () => {
+const MyCart = (props) => {
+    const user = props.route.params.user;
+    const [Cart,setCart]= React.useState(user.MyCart)
+    const [Orders,setOrders]= React.useState([])
+    let check=[]
+    const deleteCart=(position)=> {
+        let arr=[]
+        Cart.forEach((doc,i)=> {
+            if(i==position){
+                
+            }else{
+                arr.push(doc)
+            }
+        })
+        setCart(arr)
+    }
+    const selectCart=(position)=>{
+        
+        Cart.forEach((doc,i)=>{
+            if(i==position){
+                setOrders([...Orders,doc])
+            }
+        })
+        console.log(Orders)
+    }
+    const unselectCart=(position)=>{
+        let arr=[]
+        Orders.forEach((doc,i)=>{
+            if(i==position){
+
+            }else{
+               arr.push(doc) 
+            }
+        })
+        setOrders(arr)
+        console.log(Orders)
+    }
+    
     return (
         <ScrollView>
-            <ProductCart/>
-            <ProductCart/>
-            <ProductCart/>
-            <ProductCart/>
+            {
+                Cart.map((doc,i)=>(
+                    <ProductCart key={i} id={i} delete={(k)=>deleteCart(k)} data={doc}
+                        select={(n)=>selectCart(n)} unselect={(n)=>unselectCart(n)}
+                    />
+                ))
+            }
             <View style={model.viewBox}>
             <Button style={model.button} mode='contained'>Order Now</Button>
             </View>
         </ScrollView>
     );
 };
-export default MyCart;
+export default MyCart; 
